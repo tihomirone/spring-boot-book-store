4a8282eb9ba25036b77567e486b1784ef064de59
tihomir <tihomir17@yahoo.com>
2025-01-21 11:54:51 +0200

    AuthenticationService implemented.

---
+++ src/main/java/com/mj/book_seller/controller/AuthenticationController.java
@@ -0,0 +1,26 @@
+package com.mj.book_seller.controller;
+
+import com.mj.book_seller.model.UserEntity;
+import com.mj.book_seller.service.AuthenticationService;
+import com.mj.book_seller.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/authentication")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+  private final AuthenticationService authenticationService;
+  private final UserService userService;
+
+  @PostMapping("signup")
+  public ResponseEntity<UserEntity> signUp(@RequestBody UserEntity user) {
+    return new ResponseEntity<>(null, HttpStatus.OK);
+  }
+}
