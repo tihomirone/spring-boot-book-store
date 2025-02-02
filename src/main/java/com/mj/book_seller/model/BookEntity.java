b7f2f3614fce8ec1fcf7bfcf68a0721dabb8c61c
tihomirone <tihomir17@yahoo.com>
2025-02-02 21:49:33 +0200

    Add price to Book entity.

--- src/main/java/com/mj/book_seller/model/BookEntity.java
+++ src/main/java/com/mj/book_seller/model/BookEntity.java
@@ -23,6 +23,9 @@ public class BookEntity {
   @Column(name = "author", nullable = false, length = 100)
   private String author;
 
+  @Column(name = "price", nullable = false)
+  private Double price;
+
   @Column(name = "create_time", nullable = false)
   private LocalDateTime createTime;
 }
