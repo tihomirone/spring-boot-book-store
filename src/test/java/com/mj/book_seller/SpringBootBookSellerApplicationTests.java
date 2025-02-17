41550968b3bd6db504b3152d3ac023138b90e618
tihomirone <tihomir17@yahoo.com>
2025-02-17 12:15:58 +0200

    Automatic test is reverted back.

--- src/test/java/com/mj/book_seller/SpringBootBookSellerApplicationTests.java
+++ src/test/java/com/mj/book_seller/SpringBootBookSellerApplicationTests.java
@@ -1,12 +1,12 @@
 package com.mj.book_seller;
 
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class SpringBootBookSellerApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
 	}
 
